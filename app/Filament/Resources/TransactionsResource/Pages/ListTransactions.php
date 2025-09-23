19eebe354f5ca4763d4a97de100ddef3e460c3dc
AnggaRiyandiSaputra <anggariyandisaputra19@gmail.com>
2025-09-23 18:14:05 +0700

    feat(ListTransactions) add tabs for categorizing transactions by finance category

--- app/Filament/Resources/TransactionsResource/Pages/ListTransactions.php
+++ app/Filament/Resources/TransactionsResource/Pages/ListTransactions.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Resources\TransactionsResource\Pages;
 
-use App\Filament\Resources\TransactionsResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+
+use Filament\Resources\Pages\ListRecords\Tab;
+use App\Filament\Resources\TransactionsResource;
 
 class ListTransactions extends ListRecords
 {
@@ -14,6 +16,16 @@ class ListTransactions extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            null =>Tab::make('All'),
+            'Operasional'=> Tab::make()->query(fn($query)=> $query->where('categorie_finance_id',1)),
+            'Kas' => Tab::make()->query(fn($query)=> $query->where('categorie_finance_id',2)),
+            'Pajak' => Tab::make()->query(fn($query)=> $query->where('categorie_finance_id',3)),
         ];
     }
 }
